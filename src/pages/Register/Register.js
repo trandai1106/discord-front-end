6369955a1e0b22783a3265b3ebabef1d4b288662
xd3011 <xuandat3011@gmail.com>
2023-06-19 23:00:29 +0700

    Fix chat view

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,19 +1,19 @@
-import classNames from "classnames/bind";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faKey } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { useNavigate } from "react-router-dom";
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
-import styles from "./Register.module.scss";
+import styles from './Register.module.scss';
 import authAPI from '../../api/authAPI';
 
 const cx = classNames.bind(styles);
 
 function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const navigate = useNavigate();
 
   const isValidPassword = (password) => {
@@ -23,27 +23,27 @@ function Register() {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (username === "") {
+    if (username === '') {
       return;
     }
 
     if (!isValidPassword(password)) {
-      alert("Password length must be 8 to 16 characters and not contain any space");
+      alert('Password length must be 8 to 16 characters and not contain any space');
       return;
-    };
+    }
 
     if (password !== confirmPassword) {
-      alert("Please confirm your password");
+      alert('Please confirm your password');
       return;
     }
 
     const res = await authAPI.register({
-      username: username,
+      name: username,
       password: password,
     });
 
     if (res.status === 1) {
-      navigate("/chat");
+      navigate('/chat');
     } else {
       alert(res.message);
     }
@@ -51,8 +51,8 @@ function Register() {
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
-    if (e.target.value === "") {
-      e.target.placeholder = "* Please enter your username";
+    if (e.target.value === '') {
+      e.target.placeholder = '* Please enter your username';
       e.target.parentNode.classList.add(cx('error'));
     } else {
       e.target.parentNode.classList.remove(cx('error'));
@@ -61,8 +61,8 @@ function Register() {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
-    if (e.target.value === "") {
-      e.target.placeholder = "* Please enter your password";
+    if (e.target.value === '') {
+      e.target.placeholder = '* Please enter your password';
       e.target.parentNode.classList.add(cx('error'));
     } else {
       e.target.parentNode.classList.remove(cx('error'));
@@ -71,8 +71,8 @@ function Register() {
 
   const handleConfirmPasswordChange = (e) => {
     setConfirmPassword(e.target.value);
-    if (e.target.value === "") {
-      e.target.placeholder = "* Please confirm your password";
+    if (e.target.value === '') {
+      e.target.placeholder = '* Please confirm your password';
       e.target.parentNode.classList.add(cx('error'));
     } else {
       e.target.parentNode.classList.remove(cx('error'));
@@ -94,17 +94,26 @@ function Register() {
           </div>
           <div className={cx('input-container')}>
             <FontAwesomeIcon className={cx('input-icon')} icon={faKey} />
-            <input type="password" value={confirmPassword} onChange={handleConfirmPasswordChange} placeholder="Confirm your password"></input>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              placeholder="Confirm your password"
+            ></input>
           </div>
           <div className={cx('btn-container')}>
-            <button className={cx('btn', 'active')} type="submit">Register</button>
+            <button className={cx('btn', 'active')} type="submit">
+              Register
+            </button>
             <div className={cx('spread')}>Already have an account?</div>
-            <button className={cx('btn')} onClick={() => navigate("/login")}>Login</button>
+            <button className={cx('btn')} onClick={() => navigate('/login')}>
+              Login
+            </button>
           </div>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
 export default Register;
